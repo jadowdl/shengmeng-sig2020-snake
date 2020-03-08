98f7bf68ce775556cd12acee95feb83b6eb8ab40
James Dowdell <jdowdell@gmail.com>
2020-03-07 22:50:05 -0800

    Update Model.js for snake instead of Lights Out

--- Model.js
+++ Model.js
@@ -1,27 +1,51 @@
-const GRID_WIDTH = 4;
-const GRID_HEIGHT = 4;
+const GRID_WIDTH = 10;
+const GRID_HEIGHT = 10;
+
+const EMPTY_TYPE = 0;
+const SNAKE_TYPE = 1;
 
 class Model {
+
+    // "Source of Truth"
+    snake = [];   // example: [[1,1], [2,1], [2,2]]
+
+    // "Derived Data"
     rows = [];
 
     constructor(str) {
         if (str) {
-            this.rows = JSON.parse(str);
+            this.snake = JSON.parse(str);
+            this.deriveRows();
         } else {
-            this.initializeBoard();
+            this.initializeForGameStart();
         }
     }
 
-    initializeBoard() {
+    deriveRows() {
       this.rows = [];
+
+      // Initialize to all EMPTY
       for(var row = 0; row < GRID_HEIGHT; row += 1) {
-        var cols = [];
+        var columns = [];
         for(var col = 0; col < GRID_WIDTH; col += 1) {
-          cols.push(false);
+          columns.push(EMPTY_TYPE);
         }
-        this.rows.push(cols);
+        this.rows.push(columns);
       }
-      
+
+      // example snake == [[1,1], [2,1], [2,2]]
+      // Overwrite some grid cells with SNAKE type
+      // ("color the table cell green")
+      for(var snake_cell = 0; snake_cell < this.snake.length; snake_cell+=1) {
+         const [row, col] = this.snake[snake_cell];
+         const columns = this.rows[row];
+         columns[col] = SNAKE_TYPE;
+      }
+    }
+
+    initializeForGameStart() {
+      this.snake = [[5, 5], [5, 6]];
+      this.deriveRows();
     }
 
     at(row, col) {
@@ -29,10 +53,14 @@ class Model {
     }
 
     handleClickAt(row, col) {
+
+      // This was for Lights Out.
+      // Doesn't make sense for snake currently.
+
       // hack for wrap around.
+      /*
       row += GRID_HEIGHT;
       col += GRID_WIDTH;
-
       const board = this.rows;
       const offsets = [[0,0], [0, 1], [0, -1], [1, 0], [-1, 0]];
       for (var i = 0; i < offsets.length; i+=1) {
@@ -41,11 +69,12 @@ class Model {
         const r_index = (row+x)%GRID_HEIGHT;
         const c_index = (col+y)%GRID_WIDTH;
         this.rows[r_index][c_index] = !this.rows[r_index][c_index];
-      }
+      }*/
     }
 
     serialize() {
-        return JSON.stringify(this.rows);
+        // UNDOES THIS: this.snake = JSON.parse(str);
+        return JSON.stringify(this.snake);
     }
 }
 
