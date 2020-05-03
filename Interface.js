ca30285ae2d086711bf7732f655a277f19f69311
James Dowdell <jdowdell@gmail.com>
2020-05-02 22:24:35 -0700

    just some comments

--- Interface.js
+++ Interface.js
@@ -1,7 +1,14 @@
 const PULSE_TIME = 20; // milliseconds
 
+// Some chatter that Contants.manifest needs to come from a different package?
 import Constants from "expo-constants";
 const { manifest } = Constants;
+
+// https://docs.expo.io/workflow/how-expo-works/
+// https://create-react-app.dev/docs/proxying-api-requests-in-development/ - doesn't work in
+// expo, but would have been nice.
+// Instead https://stackoverflow.com/questions/61569586/unifying-localhost-dev-api-server-access-for-expo-app-across-android-ios-and-w.  Not sure how this works with prod environment, but for
+// web we'll use webpack proxy, and otherwise just call directly to same machine, different port.
 
 const SERVER_URL = (() => {
   // TODO - put a "prod" api server somewhere
